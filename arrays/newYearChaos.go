16493b2008a3c77f0e482f0ac55f2db669193a1b
aiwanesk <alexandre.iwanesko@gmail.com>
2024-02-24 14:48:44 +0100

    Update newYearChaos.go

--- arrays/newYearChaos.go
+++ arrays/newYearChaos.go
@@ -48,7 +48,6 @@ func minimumBribes(q []int32) {
         if q[i] != int32(i+1) {
             if i-1 >= 0 && q[i-1] == int32(i+1) {
                 cnt++
-                // Swapping in Go
                 q[i], q[i-1] = q[i-1], q[i]
             } else if i-2 >= 0 && q[i-2] == int32(i+1) {
                 cnt += 2
